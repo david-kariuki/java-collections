837a8af63433c4c5f2c9fd831c76bfbeff42523b
dk <workplace.davidkariuki@gmail.com>
2023-01-11 10:58:24 +0300

    feat: Template class Create template class
    Signed-off-by: dk <workplace.davidkariuki@gmail.com>

---
+++ src/main/java/iterator/TemplateClass.java
@@ -0,0 +1,21 @@
+/**
+ * @note
+ * @note
+ * @author David Kariuki
+ * @since /01/2023
+ */
+
+package iterator;
+
+
+public class TemplateClass {
+
+    /**
+     * Main Method
+     *
+     * @param args - String array
+     */
+    public static void main(String[] args) {
+
+    }
+}
