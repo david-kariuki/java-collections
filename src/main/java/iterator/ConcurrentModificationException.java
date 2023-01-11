59fc36eff8b1d60a196559d814df941cfbe58889
dk <workplace.davidkariuki@gmail.com>
2023-01-11 11:19:46 +0300

    feat: Create class to test for ConcurrentModificationException
    Signed-off-by: dk <workplace.davidkariuki@gmail.com>

---
+++ src/main/java/iterator/ConcurrentModificationException.java
@@ -0,0 +1,42 @@
+/**
+ * @note Some collections do not allow you to modify the collection while you are iterating it via an Iterator. In that case you will get a ConcurrentModificationException the next time you call the Iterator next() method.
+ * The following example results in a ConcurrentModificationException when executed:
+ * @author David Kariuki
+ * @since 11/01/2023
+ */
+
+package iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@SuppressWarnings({"WhileLoopReplaceableByForEach"})
+public class ConcurrentModificationException {
+
+    /**
+     * Main Method
+     *
+     * @param args - String array
+     */
+    public static void main(String[] args) {
+
+        // Create List
+        List<Integer> numberList = new ArrayList<>();
+        numberList.add(123);
+        numberList.add(456);
+        numberList.add(789);
+
+        Iterator<Integer> numberListIterator = numberList.iterator(); // Get list iterator
+
+        // Iterate iterator
+        while (numberListIterator.hasNext()) {
+
+            Integer value = numberListIterator.next();
+
+            if (value.equals(456)) {
+                numberList.add(111);
+            }
+        }
+    }
+}
