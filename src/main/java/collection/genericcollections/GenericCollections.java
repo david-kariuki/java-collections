98ec70ee753243b129f663573554418b5ba03a09
dk <workplace.davidkariuki@gmail.com>
2023-01-12 17:02:59 +0300

    feat: Create class file
    Create class to demonstrate java generic collections
    
    Signed-off-by: dk <workplace.davidkariuki@gmail.com>

---
+++ src/main/java/collection/genericcollections/GenericCollections.java
@@ -0,0 +1,19 @@
+package collection.genericcollections;
+
+/**
+ * @author David Kariuki
+ * @note
+ * @since 12/01/2023
+ */
+
+public class GenericCollections {
+
+    /**
+     * Main Method
+     *
+     * @param args - String array
+     */
+    public static void main(String[] args) {
+
+    }
+}
