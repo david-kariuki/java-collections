03492e9a101f894a590b1f5c471cc7d75b129013
David Kariuki <dkaris.k@gmail.com>
2023-01-17 00:37:19 +0300

    feat: sort() method
    Create class to demonstrate `sort()` java collection method

---
+++ src/main/java/collection/methodsofthejavacollection/Sort.java
@@ -0,0 +1,36 @@
+package collection.methodsofthejavacollection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @note Class to demonstrate the sort() collection method
+ * @note The Collections sort() method can sort a Java List.
+ * @author David Kariuki
+ * @since 17/01/2023
+ */
+public class Sort {
+
+  /**
+   * Main Method
+   *
+   * @param args - String array
+   */
+  public static void main(String[] args) {
+
+    List<String> list = new ArrayList<String>();
+    list.add("one");
+    list.add("two");
+    list.add("three");
+    list.add("four");
+
+    System.out.println("List before sorting\n");
+    list.forEach(System.out::println);
+
+    Collections.sort(list); // Sort list - Alphabetically
+
+    System.out.println("\nList after sorting\n");
+    list.forEach(System.out::println);
+  }
+}
