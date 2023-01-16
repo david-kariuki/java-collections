86df51e49b660731f09d14c73550d0d7d5582c6a
David Kariuki <dkaris.k@gmail.com>
2023-01-17 00:39:06 +0300

    feat: replaceAll() method
    Create class to demonstrate `replaceAll()` java collection method
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

---
+++ src/main/java/collection/methodsofthejavacollection/ReplaceAll.java
@@ -0,0 +1,37 @@
+package collection.methodsofthejavacollection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @note Class to demonstrate the replaceAll() collection method
+ * @note The Java Collections replaceAll() method can replace all occurrences of one element with
+ *     another element.
+ * @author David Kariuki
+ * @since 17/01/2023
+ */
+public class ReplaceAll {
+
+  /**
+   * Main Method
+   *
+   * @param args - String array
+   */
+  public static void main(String[] args) {
+
+    List<String> source = new ArrayList<>(3);
+    source.add("A");
+    source.add("B");
+    source.add("A");
+
+    System.out.println("List before replacing\n");
+    source.forEach(System.out::println);
+
+    boolean replacedAny = Collections.replaceAll(source, "A", "C");
+
+    System.out.println("\nList after replacing\n");
+    source.forEach(System.out::println);
+    System.out.println("\nList elements replaced : " + replacedAny);
+  }
+}
