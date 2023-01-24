707bf2b7a1eb96ba3f3d20a8b8785a52299ba728
David Kariuki <dkaris.k@gmail.com>
2023-01-24 07:22:56 +0300

    feat: Set method
    Create class to demonstrate set example
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

---
+++ src/main/java/e_set/SetExample.java
@@ -0,0 +1,16 @@
+package e_set;
+
+/**
+ * @note Class to demonstrate an example of Set
+ * @author David Kariuki
+ * @since 24/01/2023
+ */
+public class SetExample {
+
+  /**
+   * Main Method
+   *
+   * @param args - String array
+   */
+  public static void main(String[] args) {}
+}
