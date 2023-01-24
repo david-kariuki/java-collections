de6a1cd9f5fb69b61d8074563ffae9a5d419cfc3
David Kariuki <dkaris.k@gmail.com>
2023-01-24 07:24:32 +0300

    refactor: Minor change
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

--- src/main/java/TemplateClass.java
+++ src/main/java/TemplateClass.java
@@ -1,17 +1,28 @@
-/**
- * @note
- * @author David Kariuki
- * @since 11/01/2023
- */
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
+/**
+ * @note A class to demonstrate
+ * @author David Kariuki
+ * @since 24/01/2023
+ */
 public class TemplateClass {
 
-    /**
-     * Main Method
-     *
-     * @param args - String array
-     */
-    public static void main(String[] args) {
+  /**
+   * Main Method
+   *
+   * @param args - String array
+   */
+  @SuppressWarnings("rawtypes")
+  public static void main(String[] args) {
 
+    Set hashSet = new HashSet();
+    Set<String> stringsHashSet = new HashSet<>();
+    Set linkedHashSet = new LinkedHashSet();
+    Set<String> stringsLinkedHashSet = new LinkedHashSet<>();
+    Set treeSet = new TreeSet();
+    Set<String> stringsTreeSet = new TreeSet<>();
     }
 }
