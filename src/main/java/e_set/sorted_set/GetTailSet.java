acd2d82a0a895143d136f2aa20460724b0d1613e
David Kariuki <dkaris.k@gmail.com>
2023-02-03 06:25:54 +0300

    feat: Set
    Create class to demonstrate how to implement and get a tail-set from a SortedSet
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

---
+++ src/main/java/e_set/sorted_set/GetTailSet.java
@@ -0,0 +1,35 @@
+package e_set.sorted_set;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * @note A class to demonstrate how to implement a TreeSet
+ * @author David Kariuki
+ * @since 01/02/2023
+ */
+public class GetTailSet {
+
+  /**
+   * Main Method
+   *
+   * @param args - String array
+   */
+  @SuppressWarnings("WhileLoopReplaceableByForEach")
+  public static void main(String[] args) {
+
+    SortedSet<String> sortedSet = new TreeSet<>();
+    Collections.addAll(sortedSet, "a", "b", "c", "d", "e", "f");
+
+    SortedSet<String> tailSet = sortedSet.tailSet("d"); // Get tail-set
+    Iterator<String> headSetIterator = tailSet.iterator(); // Get iterator
+
+    // Loop iterator
+    while (headSetIterator.hasNext()) {
+      String element = headSetIterator.next();
+      System.out.println(element);
+    }
+  }
+}
