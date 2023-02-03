3f31d9125c8c22127eefdf01a1650d38348dd42c
David Kariuki <dkaris.k@gmail.com>
2023-02-03 06:24:49 +0300

    feat: Set
    Create class to demonstrate how to implement and use a TreeSet with comparator
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

---
+++ src/main/java/e_set/sorted_set/TreeSetWithComparator.java
@@ -0,0 +1,43 @@
+package e_set.sorted_set;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+/**
+ * @note A class to demonstrate how to implement a TreeSet
+ * @author David Kariuki
+ * @since 01/02/2023
+ */
+public class TreeSetWithComparator {
+
+  /**
+   * Main Method
+   *
+   * @param args - String array
+   */
+  public static void main(String[] args) {
+
+    Comparator<String> comparator = String::compareTo;
+
+    TreeSet<String> treeSet = new TreeSet<>(comparator);
+    treeSet.add("One");
+    treeSet.add("Two");
+    treeSet.add("Three");
+
+    Iterator<String> ascendingIterator = treeSet.iterator();
+    Iterator<String> descendingIterator = treeSet.descendingIterator();
+
+    System.out.println("Ascending iterator");
+    while (ascendingIterator.hasNext()) {
+      String element = ascendingIterator.next();
+      System.out.println(element);
+    }
+
+    System.out.println("Descending iterator");
+    while (descendingIterator.hasNext()) {
+      String element = ascendingIterator.next();
+      System.out.println(element);
+    }
+  }
+}
